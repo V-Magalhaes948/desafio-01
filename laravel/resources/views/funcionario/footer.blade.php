9163e1279418ffb27ad2b704f17245aed6fae87c
Vinicius Dos Santos Magalhães Filho <vinimedfilho@gmail.com>
2024-06-30 18:50:29 -0300

    WIP

--- laravel/resources/views/funcionario/footer.blade.php
+++ laravel/resources/views/funcionario/footer.blade.php
@@ -1,4 +1,5 @@
 
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/5.0.0/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
--- laravel/resources/views/funcionario/header.blade.php
+++ laravel/resources/views/funcionario/header.blade.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>desafio-01</title>
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/5.0.0/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>
     <div>
