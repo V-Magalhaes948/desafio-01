3030b7712372d0e15673c2c700d1285d0f094eff
Vinicius Dos Santos Magalhães Filho <vinimedfilho@gmail.com>
2024-07-01 12:46:58 -0300

    Organizando as rotas.

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -6,8 +6,11 @@ use App\Http\Controllers\PerfilFuncionarioController;
 
 
 
+// Rota inicial
+Route::get('/', [FuncionarioController::class, 'index'])->name('funcionario.index');
+
+// Rotas para Funcionário
 Route::prefix('funcionario')->name('funcionario.')->group(function () {
-    Route::get('/', [FuncionarioController::class, 'index'])->name('index');
     Route::get('/cadastro', [FuncionarioController::class, 'create'])->name('create');
     Route::post('/', [FuncionarioController::class, 'store'])->name('store');
     Route::get('/{id}/edit', [FuncionarioController::class, 'edit'])->name('edit');
@@ -15,7 +18,7 @@ Route::prefix('funcionario')->name('funcionario.')->group(function () {
     Route::delete('/{id}', [FuncionarioController::class, 'destroy'])->name('destroy');
 });
 
-
+// Rotas para Perfil de Funcionário
 Route::prefix('funcionarioperfil')->name('funcionarioperfil.')->group(function () {
     Route::get('/{id}/cadastro', [PerfilFuncionarioController::class, 'create'])->name('create');
     Route::post('/{id}', [PerfilFuncionarioController::class, 'store'])->name('store');
@@ -24,4 +27,3 @@ Route::prefix('funcionarioperfil')->name('funcionarioperfil.')->group(function (
     Route::put('/{id}', [PerfilFuncionarioController::class, 'update'])->name('update');
     Route::delete('/{id}', [PerfilFuncionarioController::class, 'destroy'])->name('destroy');
 });
-
